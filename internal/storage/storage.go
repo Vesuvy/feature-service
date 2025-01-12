b0af49132557366d2468701ad0076bb2d1cadef0
Vesuvy <baenkovav@mer.ci.nsu.ru>
2025-01-12 21:01:35 +0700

    update sqlite file and storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -11,6 +11,7 @@ type Storage interface {
 	Pick(name string) (*models.Feature, error)
 	Remove(f *models.Feature) error
 	IsExists(f *models.Feature) (bool, error)
+	Update(f *models.Feature) error
 }
 
 var ErrNoSavedFeature = errors.New("no saved feature")
